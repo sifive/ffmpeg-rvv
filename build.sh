f4aba1bee0a9349ed934106cbff8984de5aafe8b
Arnie Chang <arnie.chang@sifive.com>
2022-12-13 22:47:00 -0800

    Add a script to build FFmpeg for RISCV platform

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p ffmpeg-5.0/build_riscv
+cd ffmpeg-5.0/build_riscv
+../configure --enable-cross-compile --cross-prefix=riscv64-unknown-linux-gnu-llvm- --sysroot=/nfs/teams/sw/static/frameworks/riscv_tool/toolsuite-linux/sifive-internal/2022.08.0/riscv64-unknown-linux-gnu-toolsuite-14.9.0-2022.08.0-x86_64-linux-redhat8/sysroot/ --disable-doc --target-os=linux --arch=riscv --disable-ffplay --disable-sse2 --disable-sse --disable-x86asm --disable-avx --disable-avx2 --disable-avx512 --enable-static --disable-ffprobe --extra-cflags='-O3 -mcpu=sifive-x280 -ffast-math -mllvm -riscv-v-vector-bits-min=512 -static' --cc=riscv64-unknown-linux-gnu-clang --cxx=riscv64-unknown-linux-gnu-clang++ --strip=riscv64-unknown-linux-gnu-strip --disable-decoder=aac --disable-swscale
+make clean; make -j8
